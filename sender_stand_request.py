6ca44079777e36f7a71d0a17e0ff0325fcdab938
nasur12 <nasur@ya.ru>
2024-10-07 10:37:38 +0300

    Add files via upload

---
+++ sender_stand_request.py
@@ -0,0 +1,14 @@
+# sender_stand_request.py
+import configuration
+import requests
+
+# Создание заказа
+def create_order(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREAT_ORDERS,
+                         json=body)
+
+# Получение заказа по номеру трекера
+def get_order(track_number):
+    get_order_url = f"{configuration.URL_SERVICE}{configuration.GET_ORDER_PATH}{track_number}"
+    response = requests.get(get_order_url)
+    return response
